7547bf1d19f9c1b0b8ef8a6236a4fb1cb296700f
Harshith404 <harshithshetty2006@gmail.com>
2026-06-05 01:08:17 +0530

    2nd hour starting

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require("express");
 
 const app = express();
-
+app.use(express.json());
 const PORT = 3500;
 
 app.get("/",(req,res)=>{
   res.send("Casino Backend Running");
 });
 
-app.get("/user",(req,res)=>{
+/* app.get("/user",(req,res)=>{
   res.json({
       username:"harshith",
       balance:1000
@@ -22,8 +22,8 @@ app.get("/dice",(req,res)=>{
     roll:roll
   })
 })
-
-app.get("/coinflip",(req,res)=>{
+ */
+/* app.get("/coinflip",(req,res)=>{
   const win = Math.random()>0.45;
   let coin;
   if (win){
@@ -35,7 +35,36 @@ app.get("/coinflip",(req,res)=>{
   res.json({
     Coin:coin
   })
+}) */
+
+app.post("/coinflip",(req,res)=>{
+
+  const bet = req.body.bet;
+  const guess = req.body.guess;
+  const coin = Math.random();
+  let face;
+  if (coin>0.5){
+    face="Heads";
+  }
+  else{
+    face="Tails";
+  }
+  if(guess===face){
+    res.json({
+      "won":true,
+      "coin":face,
+      "profit":bet
+    })
+  }
+    else{
+      res.json({
+        "won":false,
+        "coin":face,
+        "profit":-bet
+      })
+    }
 })
+
 app.listen(PORT,()=>{
   console.log( `Server running on ${PORT}`)
 });
