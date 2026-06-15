5ce8bad890d35e5e4db4f0b3075489de5972125e
Harshith404 <harshithshetty2006@gmail.com>
2026-06-15 20:40:45 +0530

    Added / Route

--- server.js
+++ server.js
@@ -4,11 +4,15 @@ connectDB();
 const express = require("express");
 const app = express();
 app.use(express.json());
-const PORT = 3500;
+const PORT = process.env.PORT || 3500;
 
 const { loginUser , registerUser, getUser, coinFlip,deposit, withdraw, leaderboard } = require("./controllers/userController");
 const userRoutes = require("./routes/userRoutes");
 app.use("/", userRoutes);
+
+app.get("/", (req,res)=>{
+    res.send("Casino Backend Running");
+});
 
 app.listen(PORT,()=>{
   console.log( `Server running on ${PORT}`)
