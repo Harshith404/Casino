15e264d61514518194d4d87e83e329cfc09cf94d
Harshith404 <harshithshetty2006@gmail.com>
2026-06-17 15:17:46 +0530

    Added coin flip game

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,6 +10,13 @@ function App()
   const [balance,setBalance] = useState(0);
   const [depositAmount,setDepositAmount] = useState("");
 const [withdrawAmount,setWithdrawAmount] = useState("");
+const [guess,setGuess] = useState("");
+const [betAmount,setBetAmount] = useState("");
+const [coinResult,setCoinResult] = useState("");
+const [profit,setProfit] = useState(0);
+const [won,setWon] = useState("");
+
+
 useEffect(() => {
   async function fetchUser()
   {
@@ -135,6 +142,62 @@ useEffect(() => {
     setWithdrawAmount("");
     setError("");
   }
+
+
+  async function handleCoinFlip()
+  {
+    const token = localStorage.getItem("token");
+    if(!token)
+    {
+      return;
+    }
+    if(guess === "")
+    {
+      setError("Guess to Flip a Coin");
+      setWon("");
+      setCoinResult("");
+      setProfit(0);
+      return;
+    }
+    const response = await fetch(
+      "https://casino-ubcn.onrender.com/coinflip",
+      {
+        method:"POST",
+        headers:{
+          Authorization: `Bearer ${token}`,
+          "Content-Type":"application/json"
+        },
+        body: JSON.stringify({ bet: Number(betAmount),
+          guess: guess
+        })
+        }
+    );
+    const data = await response.json();
+    if(data.error)
+    {
+      setError(data.error);
+      setWon("");
+      setCoinResult("");
+      setProfit(0);
+      return;
+    }
+    setCoinResult(data.coin);
+    setProfit(data.profit);
+    setBalance(data.balance);
+    setBetAmount("");
+    setError("");
+    setGuess("");
+     if(data.won=== true)
+          {
+            setWon("YOU WON");
+          }
+          if(data.won === false)
+          {
+            setWon("YOU LOST");
+          }
+  }
+
+
   if(isLoggedIn)
   {
     return(
@@ -156,6 +219,23 @@ useEffect(() => {
             onChange={(event)=>setWithdrawAmount(event.target.value)}
       />
       <button onClick={handleWithdraw}>Withdraw</button>
+      <br></br>
+      <br></br>
+      <h2>Flip A Coin Game:</h2>
+      <p>Bet Amount : </p>
+      <input placeholder="Amount"
+        value ={betAmount}
+        onChange={(event)=>{setBetAmount(event.target.value)}}/>
+      <br></br>
+      <button onClick={()=>{setGuess("Heads")}}>Heads</button>
+      <button onClick={()=>{setGuess("Tails")}}>Tails</button>
+      <button onClick={handleCoinFlip}>Flip Coin</button>
+      <br></br>
+      <p>Guess: {guess}</p>
+      <p>Coin Result: {coinResult}</p>
+      <p>Profit: {profit>0?"+":""}{profit}</p>
+      <p>{won}</p>
+      <br></br>
       <br></br>
       <button onClick={handleLogout}>Logout</button>
       <p>{error}</p>
