1f24e704f454aca0b980cba11489b7d41d3f06f2
Harshith404 <harshithshetty2006@gmail.com>
2026-06-18 19:28:07 +0530

    Add leaderboard page

--- frontend/src/pages/Leaderboard.tsx
+++ frontend/src/pages/Leaderboard.tsx
@@ -1,6 +1,48 @@
+import { useState, useEffect } from "react";
+
+type User ={
+    _id:string;
+    username:string;
+    balance:number;
+};
+
 function Leaderboard()
-{
-    return <h1>Leaderboard</h1>;
+{   
+    const [users,setUsers] = useState<User[]>([]);
+
+    async function fetchLeaderboard(){
+
+        const response = await fetch(
+      "https://casino-ubcn.onrender.com/leaderboard",
+      {
+        method:"GET",
+      }
+    );
+
+    const data  = await response.json();
+
+    setUsers(data);
+  }    
+    
+    useEffect(() => {fetchLeaderboard();}, []);
+    return (
+    <div>
+        <h1>Leaderboard</h1>
+     {
+      users.map((user,index) => (
+        <div key={user._id}>
+          <p>
+            #{index+1}
+            {" | "}
+            Username: {user.username}
+            {" | "}
+            Balance: {user.balance}
+          </p>
+
+          <hr />
+          </div>))
+     }
+    </div>);
 }
 
 export default Leaderboard;
